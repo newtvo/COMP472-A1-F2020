91da326cd4155e489a21c02b2edab69e8127a608
Newt vo <nhutmvo1997@gmail.com>
2020-10-15 00:46:32 -0400

    Use classification_report to calculate c,d

---
+++ GNB.py
@@ -0,0 +1,26 @@
+import numpy as np
+import sklearn
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix, classification_report
+
+from Dataset.dataset import *
+
+def load_row(var):
+    row_number = np.arrange(1, var.shape[0] + 1, dtype=np.int32)
+    result = np.column_stack((row_number, ))
+GNB = GaussianNB()
+# train Gaussian NB Model for dataset 1
+GNB.fit(feature_1, target_1)
+
+# apply validation set to test first
+validation_predict_1 = GNB.predict(validation_feature_1)
+
+
+# confusion matrix
+confusion_matrix = confusion_matrix(validation_predict_1, validation_target_1)
+classification_report = classification_report(validation_target_1, validation_predict_1, target_names=label_1)
+# print(confusion_matrix)
+print(classification_report)
+
+
+
